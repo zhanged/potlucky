df4b9a6ec2ce1b72f819d13588ac832d0adce15c
zhanged <edison.m.zhang@gmail.com>
2013-07-02 10:19:59 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Potlucky::Application.config.secret_key_base = 'df80914b0523a65678370a4dbb22d7433a43f012fd2b3b97530c0435ccf9ed99fc15a3340e159b74ab5467abdcbb8ece4e872baf398276af424da3061b00461c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Potlucky::Application.config.session_store :cookie_store, key: '_potlucky_session'
