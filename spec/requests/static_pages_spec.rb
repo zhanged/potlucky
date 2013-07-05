aae519f628d5331b52d5d1eab4e776de1678bd7d
zhanged <edison.m.zhang@gmail.com>
2013-07-05 13:48:53 -0700

    Finish layout and routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,42 +2,27 @@ require 'spec_helper'
 
 describe "Static pages" do
   
-  describe "Home page" do
-    
-    it "should have the content 'Potlucky'" do
-    	visit '/static_pages/home'
-    	expect(page).to have_content('Potlucky')
-    end
+  subject { page }
 
-    it "should have the right title" do
-    	visit '/static_pages/home'
-    	expect(page).to have_title('Potlucky | Home')
-    end
+  describe "Home page" do
+  	before { visit root_path }
+    
+    it { should have_content('Potlucky') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 
   describe "Help page" do
+  	before { visit help_path }
 
-  	it "should have the content 'Help'" do
-  		visit '/static_pages/help'
-  		expect(page).to have_content('Help')
-  	end
-      
-      it "should have the right title" do
-    	visit '/static_pages/help'
-    	expect(page).to have_title('Potlucky | Help')
-    end
+  	it { should have_content('Help') }
+  	it { should have_title(full_title('Help')) }
   end
 
   describe "About page" do
+  	before { visit about_path }
 
-  	it "should have the content 'About'" do
-	  	visit '/static_pages/about'
-	  	expect(page).to have_content('About')	
-  	end
-
-  	it "should have the right title" do
-    	visit '/static_pages/about'
-    	expect(page).to have_title('Potlucky | About')
-    end
+  	it { should have_content('About') }
+  	it { should have_title(full_title('About')) }
   end
 end
