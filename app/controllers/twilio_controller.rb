82174ea139cbf61df9d7863db97e1a96f841bc2f
zhanged <edison.m.zhang@gmail.com>
2013-09-09 23:19:43 -0700

    process text responses

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -5,12 +5,12 @@ class TwilioController < ApplicationController
     	@from = params[:From]
     	@body = params[:Body]
     	@invitation_id = @body.gsub("YES","")
-    	if @body.at(0..2) = "YES"
+    	if @body.at(0..2) == "YES"
     		@user = User.find_by(phone: @from)
     		Invitation.find_by(id: @invitation_id).update
-    		if Invitation.find_by(id: @invitation_id) = "Yes"
+    		if Invitation.find_by(id: @invitation_id) == "Yes"
 				render 'joined.xml.erb', :content_type => 'text/xml'
-			elsif Invitation.find_by(id: @invitation_id) = "NA"
+			elsif Invitation.find_by(id: @invitation_id) == "NA"
 				render 'unjoined.xml.erb', :content_type => 'text/xml'
 			end
     	else
@@ -18,9 +18,9 @@ class TwilioController < ApplicationController
     		@formatted_phone = @from.gsub("+1","")
     		User.find_by(@invitee_id).update_attribute(phone: @formatted_phone)
     		Invitation.find_by(id: @invitation_id).update
-    		if Invitation.find_by(id: @invitation_id) = "Yes"
+    		if Invitation.find_by(id: @invitation_id) == "Yes"
 				render 'joined.xml.erb', :content_type => 'text/xml'
-			elsif Invitation.find_by(id: @invitation_id) = "NA"
+			elsif Invitation.find_by(id: @invitation_id) == "NA"
 				render 'unjoined.xml.erb', :content_type => 'text/xml'
 			end
     	end
