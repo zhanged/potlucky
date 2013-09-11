cc73909043153d76bfe91edef7e5cc45720d198a
zhanged <edison.m.zhang@gmail.com>
2013-09-10 17:46:28 -0700

    more cleanup

--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -2,13 +2,14 @@ class InvitationsController < ApplicationController
 	before_action :signed_in_user
 
 	def update
+		@invitation = Invitation.find_by(id: params["id"])
 		if Invitation.find_by(id: params["id"]).status == "NA"
 			current_user.join!(params["id"])
-			Gather.find_by(params["id"]).increase_num_joining!(params["id"])
+			Gather.find_by(id: @invitation.gathering_id).increase_num_joining!(params["id"])
 			redirect_to root_url			
 		else
 			current_user.unjoin!(params["id"])
-			Gather.find_by(params["id"]).decrease_num_joining!(params["id"])
+			Gather.find_by(id: @invitation.gathering_id).decrease_num_joining!(params["id"])
 			redirect_to root_url
 		end
 	end
