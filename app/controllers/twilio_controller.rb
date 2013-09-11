78e78c526cc65260e7f8dbbd61271db1e2a564e3
zhanged <edison.m.zhang@gmail.com>
2013-09-10 20:03:35 -0700

    respond to text

--- app/controllers/twilio_controller.rb
+++ app/controllers/twilio_controller.rb
@@ -9,7 +9,6 @@ class TwilioController < ApplicationController
     	@invitation = Invitation.find_by(id: @invitation_id)
     	if @body.at(0..2) == "YES"
     		@user = User.find_by(phone: @formatted_phone)
-    		#@user.join!(Invitation.find_by(id: @invitation_id))
     		@invitation.update_for_text!(@invitation_id, @user)
     		if Invitation.find_by(id: @invitation_id) == "Yes"
 				render 'joined.xml.erb', :content_type => 'text/xml'
@@ -22,7 +21,6 @@ class TwilioController < ApplicationController
     		@invitee_id = Invitation.find_by(id: @invitation_id).invitee_id
     		User.find_by(id: @invitee_id).update_attribute(phone: @formatted_phone)
     		@user = User.find_by(phone: @formatted_phone)
-    		#Invitation.find_by(id: @invitation_id).update
     		@invitation.update_for_text!(@invitation_id, @user)
     		if Invitation.find_by(id: @invitation_id) == "Yes"
 				render 'joined.xml.erb', :content_type => 'text/xml'
@@ -32,6 +30,6 @@ class TwilioController < ApplicationController
 				puts "Just unjoined!"
 			end
     	end
-#    	render 'twilio_test.xml.erb', :content_type => 'text/xml'
+    	render 'twilio_test.xml.erb', :content_type => 'text/xml'
 	end
 end
