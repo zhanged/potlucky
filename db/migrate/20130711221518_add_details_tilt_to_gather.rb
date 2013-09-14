3869ad06a76b5ccac5f6f63712bc202444fd27a4
zhanged <edison.m.zhang@gmail.com>
2013-09-13 21:43:23 -0700

    change details to text

--- db/migrate/20130711221518_add_details_tilt_to_gather.rb
+++ db/migrate/20130711221518_add_details_tilt_to_gather.rb
@@ -1,6 +1,6 @@
 class AddDetailsTiltToGather < ActiveRecord::Migration
   def change
-    add_column :gathers, :details, :string
+    add_column :gathers, :details, :text
     add_column :gathers, :tilt, :integer
     add_column :gathers, :num_invited, :integer
     add_column :gathers, :num_joining, :integer, default: 1
